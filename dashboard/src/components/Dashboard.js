f2b4572e10ee3457108b6289d071039b30ccce44
Anjali <ontipulianjali@gmail.com>
2024-12-03 22:09:20 +0530

    add new files

---
+++ dashboard/src/components/Dashboard.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Route } from 'react-router-dom';
+import { Routes } from 'react-router-dom';
+
+import App from "./Apps";
+import Funds from "./Funds";
+import Holdings from "./Holdings";
+
+import Orders from "./Orders";
+import Positions from "./Positions";
+import Summary from "./Summary";
+import WatchList from "./WatchList";
+
+
+ const Dashboard = () => {
+    return (
+        <div className='dashboard-container'>
+            <WatchList />
+            <div className='content'>
+                <Routes>
+                    <Route exact path="/" element={<Summary />} />
+                    <Route exact path="/orders" element={<Orders />} />
+                    <Route exact path="/holdings" element={<Holdings />} />
+                    <Route exact path="/positions" element={<Positions />} />
+                    <Route exact path="/funds" element={<Funds />} />
+                    <Route exact path="/app" element={<App />} />
+                </Routes>
+            </div>
+        </div>
+    );
+};
+
+export default Dashboard;
---
+++ dashboard/src/components/Orders.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+
+const Orders = () => {
+    return (
+        <>
+          <div className='order-table'>
+            <table>
+                <tr>
+                    <th>Orders</th>
+                    <th>GTT</th>
+                    <th>Baskets</th>
+                    <th>SlP</th>
+                    <th>Alerts</th>
+                    <th>IPO</th>
+                </tr>
+            </table>
+        </div>
+     </>
+    );
+};
+
+export default Orders;
